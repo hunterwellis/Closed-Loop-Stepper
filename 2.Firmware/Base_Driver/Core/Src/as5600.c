ace8de105e31747c52271325d5ee818acc0b882b
Hunter Ellis <elliswhunter@gmail.com>
2024-11-18 11:11:35 -0500

    updated BOM

--- 2.Firmware/Base_Driver/Core/Src/as5600.c
+++ 2.Firmware/Base_Driver/Core/Src/as5600.c
@@ -11,12 +11,13 @@
 // Read angle
 float AS5600_Angle(I2C_HandleTypeDef *hi2c1){
 
-	//enc->rawValue = ((AS5600_Reg(hi2c1, enc, RAW_ANGLE_MSBS) & 0x0F) << 8) | AS5600_Reg(hi2c1, enc, RAW_ANGLE_LSBS);
+	// most significant 4 bits from register 0x0C
+	// least significant 8 bits from register 0x0D
 	uint8_t msbs = AS5600_Reg(hi2c1, RAW_ANGLE_MSBS);
 	uint8_t lsbs = AS5600_Reg(hi2c1, RAW_ANGLE_LSBS);
 
     // Combine the MSB and LSB to form the 12-bit raw angle value
-    uint16_t rawValue = ((msbs & 0x0F) << 8) | lsbs;
+	uint16_t rawValue = ((msbs & 0x0F) << 8) | lsbs;
     return (rawValue * 360.0f) / 4096.0f;
 
 }
@@ -28,7 +29,7 @@ uint8_t AS5600_Reg(I2C_HandleTypeDef *hi2c1, uint8_t address){
 	if ( HAL_I2C_Master_Transmit(hi2c1, AS5600_ADDR, &address, 1, HAL_MAX_DELAY) != HAL_OK ) {
 		return 0xFF;
 	} else {
-		// Read 2 bytes from the encoder register
+		// Read 8 bits from the encoder register
 		if ( HAL_I2C_Master_Receive(hi2c1, AS5600_ADDR, &data, 1, HAL_MAX_DELAY) != HAL_OK ) {
 			return 0xFF;
 		} else {
